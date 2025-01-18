79f65984aef217066e21017aaffe365b0d6855cf
marwi07 <marie.wittkee@gmail.com>
2025-01-18 10:29:36 +0100

    fixes to workform redner and edit

--- src/utility/validateEachImageUpload.js
+++ src/utility/validateEachImageUpload.js
@@ -21,25 +21,27 @@ export const validateEachImageEditUpload = async (ctx, formData, id) => {
   let thumbnailError = "";
   const images = [];
   const imageNum = [];
+
   for (let i = 1; i <= 3; i++) {
     const image = formData.get(`image${i}`);
     if (image && image.size > 0) {
       thumbnailError = validateImage.validateImage(image);
       images.push(image);
       imageNum.push(i - 1);
-    } else {
-      continue;
     }
   }
 
-  //deleting the image at the position the image was shown on site to replace with new
   const imagesInData = await model.getImagesById(ctx.db, id);
-  for (let i = 1; i <= 3; i++) {
-    if (i - 1 === imageNum[0]) {
-      const image = await model.getImageById(ctx.db, imagesInData[i - 1][5]);
-      await model.deleteWorkImageById(ctx.db, image[0][5]);
+
+  for (const position of imageNum) {
+    if (imagesInData[position]) {
+      const imageId = imagesInData[position][5];
+      if (imageId) {
+        await model.deleteWorkImageById(ctx.db, imageId);
+      }
     }
   }
+
   const imageData = { images: images, error: thumbnailError };
   return imageData;
 };
